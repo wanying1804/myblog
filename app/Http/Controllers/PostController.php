dbb7fd2f36139cabbf9466ab3e3583434ad310ce
Cassie Yang <wanying1804@gmail.com>
2017-07-27 12:42:45 +1200

    delete testing lines

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,36 +16,11 @@ class PostController extends Controller
          dd($posts->insert(['title' => 'every day is adfasgasfdadasf', 'content' => 'asdfasdf;jgweerwerdfad','publish_at' => date('Y-m-d H:i:s'), 'author_id' => 2]));
 
 
-         #添加一条数据 ture 添加成功
-         //dd($users->insert(['title' => 'email', 'article' => 'john@example.com','time' => time()]));
-         //dd($users->insert(['title' => 'huangwei', 'article' => 'huangwei@example.com','time' => time()]));
-
-         #添加多条数据 ture 添加成功
-         //dd($users->insert([['title' => 'email', 'article' => 'john@example.com','time' => time()],['title' => 'title1', 'article' => 'lichuang@example.com','time' => time()],['title' => 'title2', 'article' => 'lili@example.com','time' => time()]]));
-
-         #修改一条数据 0 修改成功
-         //dd($users->where('title', 'new2')->update(['article' => 'lichuang']));
-
-         #删除一条数据 1 为删除成功
-         //dd($users->where('title', 'new1')->delete());
-
-         #删除集合所有数据 返回几删除几条
-         //dd($users->delete());
-
-         #查询集合所有数据
-         //dd($users->get());
-
-         #按条件查询
-         //dd($users->where('title', 'title1')->get());
+     
          $postsData = $posts->where('title', 'huangwei')->get()->toArray();
          dd($postsData);
 
-         #模糊查询
-         //dd($users->where('title', 'like', 'ti%')->get());
-         //dd($users->where('title', 'like', 'ti%')->get());
-
-         #按条件排序
-         //dd($users->orderBy('time', 'desc')->get());
+ 
     }
 
     public function getContent($id)
